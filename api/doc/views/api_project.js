a2834220f6aa66bf4ded64c1eda50c30829a147d
Quentin Riviere <qriviere@silicom.fr>
2019-05-07 17:40:31 +0200

    edit doc template

--- api/doc/views/api_project.js
+++ api/doc/views/api_project.js
@@ -24,7 +24,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-05-07T14:55:52.761Z",
+    "time": "2019-05-07T15:17:28.891Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
